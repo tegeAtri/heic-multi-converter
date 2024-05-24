f4a125aebea1babf04bab8783c6f10d47404ac5c
Patrik Tegetmeier <patrik.tegetmeier@web.de>
2024-05-24 22:55:17 +0200

    check system exit tests

--- test/test_heic_multi_converter.py
+++ test/test_heic_multi_converter.py
@@ -35,21 +35,43 @@ import pytest
 from src.heic_multi_converter import main
 import shlex
 
-test_cases = [
-    (
-        "-t png --verboseOff -s .\testdata -d .\testdata",
-        "",
-    ),
+# test_cases = [
+#     (
+#         "-t png --verboseOff -s C:\git-repos\heic-multi-converter\testdataa -d C:\git-repos\heic-multi-converter\testdata",
+#         "ERROR: Source location is not existing",
+#     ),
+#     (
+#         "-t png --verboseOff -s C:\git-repos\heic-multi-converter\testdata -d C:\git-repos\heic-multi-converter\testdata",
+#         "",
+#     ),
+# ]
+
+
+# @pytest.mark.parametrize("command, expected_output", test_cases)
+# def test_not_existing_src_folder_give_an_error(capsys, command, expected_output):
+#     """
+#     This function tests that heic-multi-converter returns an error if a non-existing
+#     folder is handed over as source for photos.
+#     """
+#     main(shlex.split(command))
+#     captured = capsys.readouterr()
+#     output = captured.out + captured.err
+#     assert expected_output in output
+
+
+test_cases_sys_exit = [
+    ("", "error: the following arguments are required: -s/--src"),  # no argument passed
+    ("s", "error: the following arguments are required: -s/--src"),  # no flag passed
+    ("+s", "error: the following arguments are required: -s/--src"),  # wrong type of flag passe
+    ("-p", "error: the following arguments are required: -s/--src"),  # wrong argument name
+    ("--verboseOff --src", "ERROR: Source location is not existing"),
 ]
 
 
-@pytest.mark.parametrize("command, expected_output", test_cases)
-def test_not_existing_src_folder_give_an_error(capsys, command, expected_output):
-    """
-    This function tests that heic-multi-converter returns an error if a non-existing
-    folder is handed over as source for photos.
-    """
-    main(shlex.split(command))
-    captured = capsys.readouterr()
-    output = captured.out + captured.err
+@pytest.mark.parametrize("command, expected_output", test_cases_sys_exit)
+def test_argparse_heic_multi_converter_sys_exit(capsys, command, expected_output):
+    with pytest.raises(SystemExit):  # Expecting SystemExit due to argparse error
+        main(shlex.split(command))
+    captured = capsys.readouterr()  # Capture both stdout and stderr
+    output = captured.err + captured.out  # Combine stdout and stderr
     assert expected_output in output
